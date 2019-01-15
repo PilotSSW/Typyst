c3e2ce045822b70500dfd28927ce661785e3a972
Sean Wolford <pilotSSW@aol.com>
2019-01-14 21:20:09 -0500

    Initial Commit

---
+++ Typist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Typist
+//
+//  Created by Sean Wolford on 1/14/19.
+//  Copyright © 2019 wickedPropeller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
