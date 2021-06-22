64aa1424e2c05a99b2099a2718fad5ec7b1834de
Sean Wolford <pilotSSW@gmail.com>
2021-06-22 12:13:50 -0400

    Add enum for shadow intensity to improve UI control

---
+++ Shared/Logic/UI/SwiftUI/Styles/ShadowIntensity.swift
@@ -0,0 +1,20 @@
+//
+// Created by Sean Wolford on 6/21/21.
+//
+
+import Foundation
+
+enum ShadowIntensity: Double {
+    case none = 0.0
+    case subtle = 0.03
+    case veryLight = 0.075
+    case light = 0.125
+    case mediumLight = 0.20
+    case medium = 0.3333
+    case mediumStrong = 0.500
+    case strong = 0.6667
+    case veryStrong = 0.80
+    case intense = 0.88
+    case tooFuckingStrong = 0.95
+    case racoonEyes = 1.0
+}
