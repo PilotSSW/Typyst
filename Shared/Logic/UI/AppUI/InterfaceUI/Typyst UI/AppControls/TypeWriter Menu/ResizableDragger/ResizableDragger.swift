789cd2973eb3bae3a5e6b6b6db98c71ca74eecf2
Sean Wolford <pilotSSW@gmail.com>
2021-06-22 13:53:55 -0400

    Add message to resizable dragger on scroller
    * Add a message that displays on hover explaining how to use

--- Shared/Logic/UI/AppUI/InterfaceUI/Typyst UI/AppControls/TypeWriter Menu/ResizableDragger/ResizableDragger.swift
+++ Shared/Logic/UI/AppUI/InterfaceUI/Typyst UI/AppControls/TypeWriter Menu/ResizableDragger/ResizableDragger.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ResizableDragger: View {
+    @State var showMessage: Bool = false
     var backgroundColor: Color
 
     var body: some View {
-        Image("Icons/up-and-down-arrows")
-            .resizable()
-            .scaledToFit()
-            .frame(height: 24, alignment: .center)
-            .asStyledCardHeader(withBackgroundColor: backgroundColor, withTopPadding: 0)
+        HStack {
+            if showMessage { Spacer() }
+
+            Image("Icons/up-and-down-arrows")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 24, alignment: .center)
+                .animation(.easeOut(duration: 0.25))
+
+            if showMessage {
+                Spacer().frame(width: 12)
+                Text("Drag to resize")
+                    .asStyledText()
+                    .animation(.easeOut(duration: 0.75))
+                Spacer()
+            }
+        }
+        .asStyledCardHeader(withBackgroundColor: backgroundColor, withTopPadding: 0)
+        .onHover(perform: { isHovering in
+            showMessage = isHovering
+        })
+
     }
 }
 
